f988d007e28c7cdb8435cbef557289c515356ba8
pravini15 <pravinipw15@gmail.com>
2022-10-27 13:37:21 +0530

    initial

--- app/src/main/java/com/example/mad_project/CartActivity.java
+++ app/src/main/java/com/example/mad_project/CartActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CartActivity extends AppCompatActivity {
 
+public class CartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
     }
+
+
 }
--- app/src/main/java/com/example/mad_project/DBHelper.java
+++ app/src/main/java/com/example/mad_project/DBHelper.java
@@ -20,6 +20,18 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String COLUMN_PRICE = "price";
     private static final String COLUMN_STATUS = "status";
 
+    private static final String TABLE_DELIVERY = "delivery_details";
+    private static final String DELIVERY_ID = "billing_id";
+    private static final String DELIVERY_NAME = "billing_name";
+    private static final String CONTACT_NO = "contact_no";
+    private static final String PROVINCE = "province";
+    private static final String DISTRICT = "district";
+    private static final String CITY = "city";
+    private static final String ADDRESS = "address";
+
+
+
+
     public DBHelper(@Nullable Context context) {
         super(context, DB_NAME, null, DATABASE_VERSION);
         this.context = context;
@@ -32,11 +44,24 @@ public class DBHelper extends SQLiteOpenHelper {
                 COLUMN_PRICE + " TEXT, " +
                 COLUMN_STATUS + " TEXT);";
         db.execSQL(query);
+
+        String query1 = "CREATE TABLE "+ TABLE_DELIVERY + " (" + DELIVERY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                DELIVERY_NAME + " TEXT, " +
+                CONTACT_NO + " TEXT, " +
+                PROVINCE + " TEXT, " +
+                DISTRICT + " TEXT, " +
+                CITY + " TEXT, " +
+                ADDRESS + " TEXT);" ;
+        db.execSQL(query1);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_DELIVERY);
         onCreate(db);
     }
 
@@ -48,6 +73,26 @@ public class DBHelper extends SQLiteOpenHelper {
         cv.put(COLUMN_PRICE, price);
         cv.put(COLUMN_STATUS, status);
         long result = db.insert(TABLE_NAME, null,cv);
+        if (result == -1){
+            Toast.makeText(context, "failed", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void addBillingDetails(String billing_name, String contact_no, String province, String district, String city, String address){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(DELIVERY_NAME, billing_name);
+        cv.put(CONTACT_NO, contact_no);
+        cv.put(PROVINCE, province);
+        cv.put(DISTRICT, district);
+        cv.put(CITY, city);
+        cv.put(ADDRESS, address);
+
+
+        long result = db.insert(TABLE_DELIVERY, null,cv);
         if (result == -1){
             Toast.makeText(context, "failed", Toast.LENGTH_SHORT).show();
         }else {
