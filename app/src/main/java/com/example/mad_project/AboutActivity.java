29e1a77299c9870e249a103ab66028104083d1e6
Amaya <amayaig10@icloud.com>
2022-10-28 21:12:40 +0530

    28th Oct Commit

--- app/src/main/java/com/example/mad_project/AboutActivity.java
+++ app/src/main/java/com/example/mad_project/AboutActivity.java
@@ -1,18 +1,13 @@
 package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView(R.layout.activity_about);
     }
 }
---
+++ app/src/main/java/com/example/mad_project/ContactActivity.java
@@ -0,0 +1,19 @@
+package com.example.mad_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.widget.TextView;
+import android.window.SplashScreen;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
